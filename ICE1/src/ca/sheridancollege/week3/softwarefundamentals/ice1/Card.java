4dea34893b14dc0c2d18b6ce88345241f1750ee7
Nisarg <Nisarg@10.0.0.172>
2025-06-01 22:40:31 -0400

    Completed ICE1: Added random cards and luckyCard

--- ICE1/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ ICE1/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -17,34 +17,23 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+   private String suit;
+   private int value;
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
+    
     public String getSuit() {
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
-     */
     public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
     public void setValue(int value) {
         this.value = value;
     }
--- ICE1/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ ICE1/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -21,20 +21,17 @@ public class CardTrick {
         Card[] magicHand = new Card[7];
         Random rand = new Random();
 
-        // Fill magic hand with 7 random cards
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            c.setValue(rand.nextInt(13) + 1); // value between 1-13
-            c.setSuit(Card.SUITS[rand.nextInt(4)]); // random suit
+            c.setValue(rand.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
             magicHand[i] = c;
         }
 
-        // Hardcoded lucky card
         Card luckyCard = new Card();
         luckyCard.setValue(7);
         luckyCard.setSuit("Hearts");
 
-        // Check if lucky card is in magic hand
         boolean found = false;
         for (Card card : magicHand) {
             if (card.getValue() == luckyCard.getValue() &&
@@ -44,20 +41,17 @@ public class CardTrick {
             }
         }
 
-        // Display result
         if (found) {
             System.out.println("Lucky card found in the magic hand!");
         } else {
             System.out.println("Lucky card NOT found in the magic hand.");
         }
 
-        // Optional: Display all cards in the hand
         System.out.println("Cards in the magic hand:");
         for (Card card : magicHand) {
             System.out.println(card.getValue() + " of " + card.getSuit());
         }
 
-        // Show the lucky card
         System.out.println("Lucky Card: " + luckyCard.getValue() + " of " + luckyCard.getSuit());
     }
 }
